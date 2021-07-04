8d91bb1c29a0625487de403f26166610999ec2ee
Harshit Arora <harshit8121@gmail.com>
2021-07-05 02:21:32 +0530

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,14 @@
+import 'package:flutter/material.dart';
 
+void main() {
+  runApp(Myapp());
+}
+
+class Myapp extends StatelessWidget {
+  const Myapp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
